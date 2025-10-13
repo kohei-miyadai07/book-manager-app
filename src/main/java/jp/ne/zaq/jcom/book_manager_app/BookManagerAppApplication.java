119bd0ac89c89b86b94546e8d13a42e0afd0af6c
kohei-miyadai07 <wm25gb87@gmail.com>
2025-10-14 00:24:00 +0900

    feat: プロジェクト作成
    refs master

---
+++ src/main/java/jp/ne/zaq/jcom/book_manager_app/BookManagerAppApplication.java
@@ -0,0 +1,13 @@
+package jp.ne.zaq.jcom.book_manager_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagerAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/ne/zaq/jcom/book_manager_app/BookManagerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ne.zaq.jcom.book_manager_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
